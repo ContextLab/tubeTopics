e4e70c08887d1ecf99219ce813b872ba2af25e25
andrewheusser <andrew.heusser@gmail.com>
2016-10-07 08:50:29 -0400

    topic model working for a snippet of the video

--- examples/example.js
+++ examples/example.js
@@ -1,5 +1,7 @@
-tubeTopic = require('../index.js');
+var tubeTopics = require('../index.js');
 
-
-
-// put example here :)
+tubeTopics.getModelFromURL('https://www.youtube.com/watch?v=kpCJyQ2usJ4&list=PL1847B1B2268562C7').then(function(result){
+  console.log(result)
+})
+// https://www.youtube.com/watch?v=Xt5dRmr8bCw
+// https://www.youtube.com/watch?v=kpCJyQ2usJ4&list=PL1847B1B2268562C7
--- index.js
+++ index.js
@@ -2,98 +2,185 @@ var fs = require('fs')
 var path = require('path')
 var ytdl = require('ytdl-core');
 var ffmpeg = require('fluent-ffmpeg');
-var lda = require('lda');
-var gspeech = require('gspeech-api')
+var async = require('async');
+var math = require('mathjs')
+var google = require('googleapis');
+var speech = google.speech('v1beta1').speech;
 
-// ideas for functions:
-// 1. getTranscripts - takes a single youtube url or array of youtube urls and returns transcript of the video
-// 2. getTopics - inputs are transcript(s). option to supply your own model.  if no model is provided, the model will be based on the collection of transcripts. returns topic weights for each transcript.
+var tubeTopics = function() {
 
-module.exports = tubeTopics;
+    function loadModel() {
+        return new Promise(function(resolve, reject) {
+            fs.readFile('model/topicModelDict.json', 'utf8', function(err, data) {
+                if (err) throw err;
+                model = JSON.parse(data);
+                console.log('Using default model...')
+                resolve(model)
+            })
+        })
+    };
+    var model = loadModel()
 
-function tubeTopics(options) {
-    // optional params
-    options = options || {};
-
-    // private data
-    var audioOutputPath = opt.audioOutputPath || path.resolve(__dirname, 'sound.mp4');
-
-    function getTranscripts(urls, options) {
-        options = options || {};
-
-        if (typeof urls === 'string'){
-          urls = [urls]
-        };
-
-        var audioOutput = path.resolve(__dirname, 'sound.mp4');
-
-        urls.forEach(function(url) {
+    function getTranscript(url) {
+        return new Promise(function(resolve, reject) {
+            var audioOutput = path.resolve(__dirname, 'sound.mp4');
+            var videoTranscripts = []
+            console.log('Downloading video...')
             ytdl(url, {
                     filter: function(f) {
                         return f.container === 'mp4' && !f.encoding;
                     }
                 })
-                // Write audio to file since ffmpeg supports only one input stream.
                 .pipe(fs.createWriteStream(audioOutput))
                 .on('finish', function() {
-                    ffmpeg()
-                        .input(ytdl(url, {
-                            filter: function(f) {
-                                return f.container === 'mp4' && !f.audioEncoding;
-                            }
-                        }))
-                        .videoCodec('copy')
-                        .input(audioOutput)
-                        .audioCodec('copy')
-                        .save(path.resolve(__dirname, 'output.mp4'))
-                        .on('error', console.error)
-                        .on('progress', function(progress) {
-                            process.stdout.cursorTo(0);
-                            process.stdout.clearLine(1);
-                            process.stdout.write(progress.timemark);
-                        }).on('end', function() {
-                            gspeech.recognize(audioOutput, function(err, data) {
-                                if (err)
-                                    console.error(err);
-                                var videoTranscript = '';
-                                for (var i = 0; i < data.timedTranscript.length; i++) {
-                                    var videoTranscript = videoTranscript.concat(' ', data.timedTranscript[i].text);
-                                }
-                                videoTranscripts.push(videoTranscript)
-                            });
-                        });
-                });
-        });
-        return videoTranscripts
+                    console.log('Finished downloading. Decoding transcript...')
+                    recognize(audioOutput, console.log).then(function(transcript) {
+                        resolve(transcript)
+                    });
+
+                })
+
+        })
     };
 
-    function getTopicsFromTranscripts(transcripts,options){
-      options = options || {};
+    function getModelFromURL(url) {
+        return new Promise(function(resolve, reject) {
+            getTranscript(url).then(function(transcript) {
+                computeTopicModel(transcript, model).then(function(result) {
+                    resolve(result)
+                })
+            })
+        })
+    };
 
-      var numTopics = options.numTopics || 2;
-      var numTerms = options.numTerms || 5;
+    function computeTopicModel(words, model) {
+        return new Promise(function(resolve, reject) {
+            var topicsByWords = words.map(function(word) {
+                    return model[word]
+                })
+                .filter(function(model) {
+                    return typeof model != 'undefined'
+                })
 
-      return lda([transcripts],numTopics,numTerms)
-
+            // code for log sum exp
+            var topicsByWordsMtx = math.transpose(math.matrix(topicsByWords))
+            var a = topicsByWordsMtx['_data'].map(function(topic) {
+                return math.max(topic)
+            })
+            var topicVec = topicsByWordsMtx['_data'].map(function(topic, topicIndex) {
+                    return topic.map(function(weight) {
+                            return math.exp(weight - a[topicIndex])
+                        })
+                        .reduce(function(b, c) {
+                            return b + c
+                        })
+                })
+                .map(function(sums, idx) {
+                    return math.exp(a[idx] + math.log(sums))/topicsByWordsMtx['_data'].length
+                })
+            resolve(topicVec)
+        })
     }
 
-    function getTopicsFromURLs(urls,options){
-      options = options || {};
+    // GOOGLE SPEECH CODE
 
-      var numTopics = options.numTopics || 2;
-      var numTerms = options.numTerms || 5;
+    function getAuthClient(callback) {
+        google.auth.getApplicationDefault(function(err, authClient) {
+            if (err) {
+                return callback(err);
+            }
+            if (authClient.createScopedRequired && authClient.createScopedRequired()) {
+                authClient = authClient.createScoped([
+                    'https://www.googleapis.com/auth/cloud-platform'
+                ]);
+            }
 
-      var transcripts = getTranscripts(urls, options);
-
-      return lda([transcripts],numTopics,numTerms)
-
+            return callback(null, authClient);
+        });
     }
+
+    function prepareRequest(inputFile, options, callback) {
+        ffmpeg()
+            .input(inputFile)
+            .seekInput('1:00')
+            .format('flac')
+            .audioFrequency(16000)
+            .duration('0:0:30.00')
+            .audioChannels(1)
+            .save(inputFile + '.flac')
+            .on('end', function() {
+                fs.readFile(inputFile + '.flac', function(err, audioFile) {
+                    if (err) {
+                        return callback(err);
+                    }
+                    console.log('Got audio file!');
+                    var encoded = new Buffer(audioFile).toString('base64');
+                    var payload = {
+                        config: {
+                            encoding: 'FLAC',
+                            sampleRate: 16000
+                        },
+                        audio: {
+                            content: encoded
+                        }
+                    };
+                    return callback(null, payload);
+                })
+            })
+    };
+
+    function recognize(inputFile, callback) {
+        return new Promise(function(resolve, reject) {
+            var requestPayload;
+            async.waterfall([
+                function(cb) {
+                    prepareRequest(inputFile, cb);
+                },
+                function(payload, cb) {
+                    requestPayload = payload;
+                    getAuthClient(cb);
+                },
+                function sendRequest(authClient, cb) {
+                    console.log('Analyzing speech...');
+                    speech.syncrecognize({
+                        auth: authClient,
+                        resource: requestPayload
+                    }, function(err, result) {
+                        if (err) {
+                            return cb(err);
+                        }
+                        console.log('result:', JSON.stringify(result, null, 2));
+                        console.log(JSON.stringify(result, null, 2))
+                        words = [];
+                        if (JSON.stringify(result, null, 2) != '{}') {
+                            result.results.forEach(segment => {
+                                segment.alternatives[0].transcript.trim().split(" ").forEach(word => {
+                                    if (word != "") {
+                                        words.push(word.toUpperCase())
+                                    }
+                                })
+                            })
+                        }
+
+                        fs.writeFile(inputFile + ".txt", words, function(err) {
+                            if (err) {
+                                return console.log(err);
+                            }
+                            resolve(words)
+                            console.log("The file was saved!");
+                        });
+
+                        cb(null, result);
+                    });
+                }
+            ], callback);
+        })
+    };
 
     // API/data for end-user
     return {
-        getTranscripts: getTranscripts,
-        getTopicsFromTranscripts: getTopicsFromTranscripts,
-        getTopicsFromURLS: getTopicsFromURLS
+        getModelFromURL: getModelFromURL,
     }
-    
 }
+
+module.exports = new tubeTopics();
